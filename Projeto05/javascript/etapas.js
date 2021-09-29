77a8327552f9080244fecde095bcfdfe78b9a861
Gustavo Teixeira <89105431+GustavosTeixeira@users.noreply.github.com>
2021-09-29 14:35:28 -0300

    Add files via upload

---
+++ Projeto05/javascript/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+                numero:'38111',
+                nome: 'Maguila',
+                partido: 'SLSB',
+                fotos:[
+                    {url:'maguila.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Alcione',
+                partido: 'MSPN',
+                fotos:[
+                    {url:'alcione.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '99',
+                nome: 'Hebe Camargo', 
+                partido: 'SBT',
+                vice: 'Garrincha',
+                fotos:[
+                    {url:'hebecamargo.jpg', legenda: 'Prefeito'},
+                    {url: 'garrincha.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Jay Z',
+                partido: 'TRAP',
+                vice: 'Kanye West',
+                fotos:[
+                    {url: 'jayz.jpg', legenda: 'Prefeito'},
+                    {url: 'kanye.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
+
